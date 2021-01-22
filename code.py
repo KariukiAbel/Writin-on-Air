d0b820566e8085d6c14d410d7c16ee30c2645218
KariukiAbel <abelmuriithi@gmail.com>
2021-01-22 10:54:51 +0300

    Finished the code but now i need libgtk2.0-dev and pkg-config

--- code.py
+++ code.py
@@ -32,3 +32,49 @@ while True:
     
 stp = 0
 
+#optical flow starts here
+old_pts = np.array([[x, y]], dtype=np.float32).reshape(-1,1,2)
+mask = np.zeros_like(inp_img)
+
+while True:
+    _, new_inp_img = cap.read()
+    new_inp_img = cv2.flip(new_inp_img, 1)
+    new_gray = cv2.cvtColor(new_inp_img, cv2.COLOR_BGR2GRAY)
+    new_pts,status,err = cv2.calcOpticalFlowPyrLK(gray_inp_img, 
+                                                  new_gray, 
+                                                  old_pts, 
+                                                  None, maxLevel=1, 
+                                                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 
+                                                            15, 
+                                                            0.08))
+    
+    for i,j in zip(old_pts, new_pts):
+        x,y = j.ravel()
+        a,b = i.ravel()
+        if cv2.waitKey(2) & 0xff == ord('q'):
+            stp = 1
+            
+        elif cv2.waitKey(2) & 0xff == ord('w'):
+            stp = 0
+            
+        elif cv2.waitKey(2) == ord('n'):
+            mask = np.zeros_like(new_inp_img)
+            
+        if stp == 0:
+            mask = cv2.line(mask, (a,b), (x,y), (0,0,255), 6)
+            
+        cv2.circle(new_inp_img, (x,y), 6, (0,255,0), -1)
+    
+    new_inp_img = cv2.addWeighted(mask, 0.3, new_inp_img, 0.7, 0)
+    cv2.putText(mask, "'q' to gap 'w' - start 'n' - clear", (10,50), cv2.FONT_HERSHEY_PLAIN, 2, (255,255,255))
+    cv2.imshow("output", new_inp_img)
+    cv2.imshow("result", mask)
+    
+    gray_inp_img = new_gray_copy()
+    old_pts = new_pts.reshape(-1,1,2)
+    
+    if cv2.waitKey(1) & 0xff == 27:
+        break
+    
+cv2.destroyAllWindows()
+cap.release()
